847da70900a6b47c6584d2d56040180529553281
Victor1128 <victorbadulescu7@gmail.com>
2023-01-07 17:18:44 +0200

    schimbare email fara confirmare

--- Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -116,20 +116,18 @@ namespace OpenDiscussion.Areas.Identity.Pages.Account.Manage
             var email = await _userManager.GetEmailAsync(user);
             if (Input.NewEmail != email)
             {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                var callbackUrl = Url.Page(
-                    "/Account/ConfirmEmailChange",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = userId, email = Input.NewEmail, code = code },
-                    protocol: Request.Scheme);
-                await _emailSender.SendEmailAsync(
-                    Input.NewEmail,
-                    "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-
-                StatusMessage = "Confirmation link to change email sent. Please check your email.";
+                var check = await _userManager.FindByEmailAsync(Input.NewEmail);
+                if (check == null)
+                {
+                    await _userManager.SetEmailAsync(user, Input.NewEmail);
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+                    var result = await _userManager.ConfirmEmailAsync(user, code);
+                    StatusMessage = "Email changed!";
+                }
+                else
+                {
+                    StatusMessage = "Email already exists.";
+                }
                 return RedirectToPage();
             }
 
